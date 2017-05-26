f6c6dacdb7e6fcd2d780d781d04e4953e45ca7db
Zachary Wander <zachary@techyteen.tk>
2017-05-26 17:31:06 -0400

    Let's see if Nathan can find what I did :)

--- app/src/main/java/com/huetoyou/chatexchange/MainActivity.java
+++ app/src/main/java/com/huetoyou/chatexchange/MainActivity.java
@@ -1,13 +1,25 @@
 package com.huetoyou.chatexchange;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.Toast;
+
+import com.huetoyou.chatexchange.auth.AuthenticatorActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences mSharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSharedPrefs = getSharedPreferences(getResources().getText(R.string.app_name).toString(), MODE_PRIVATE);
+
+        startActivity(new Intent(this, AuthenticatorActivity.class));
     }
 }
