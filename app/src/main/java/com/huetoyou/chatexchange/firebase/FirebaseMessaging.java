e43df2afc2e75a9597917babc3ba43a8b9978dcb
Zachary Wander <zachary@techyteen.tk>
2017-05-30 13:12:33 -0400

    Add FCM

---
+++ app/src/main/java/com/huetoyou/chatexchange/firebase/FirebaseMessaging.java
@@ -0,0 +1,38 @@
+package com.huetoyou.chatexchange.firebase;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseMessaging extends FirebaseMessagingService {
+    private final String TAG = "ChatExchange";
+
+    public FirebaseMessaging() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
---
+++ app/src/main/java/com/huetoyou/chatexchange/firebase/FirebaseService.java
@@ -0,0 +1,26 @@
+package com.huetoyou.chatexchange.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Zacha on 5/30/2017.
+ */
+
+public class FirebaseService extends FirebaseInstanceIdService {
+    private final String TAG = "ChatExchange";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
+    }
+}
