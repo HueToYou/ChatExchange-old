20c2ba73c4208b8edf32dc47414b531b3c61dbf9
Zachary Wander <zachary@techyteen.tk>
2017-06-01 20:58:02 -0400

    Improve color change

--- app/src/main/java/com/huetoyou/chatexchange/ui/activity/PreferencesActivity.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/activity/PreferencesActivity.java
@@ -10,13 +10,21 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 import com.huetoyou.chatexchange.R;
+import com.jrummyapps.android.colorpicker.ColorPanelView;
 import com.jrummyapps.android.colorpicker.ColorPickerDialog;
+import com.jrummyapps.android.colorpicker.ColorPickerView;
+import com.jrummyapps.android.colorpicker.ColorPreference;
 
 public class PreferencesActivity extends AppCompatPreferenceActivity {
     private SharedPreferences mSharedPrefs;
+    private ColorPickerView colorPickerView;
+    private ColorPanelView newColorPanelView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,15 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            ColorPreference colorPreference = (ColorPreference) findPreference("default_color");
+            colorPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    ((PreferencesActivity)getActivity()).setActionBarColor();
+                    return true;
+                }
+            });
 
             CheckBoxPreference checkBoxPreference = (CheckBoxPreference) findPreference("dynamic_bar_color");
             setAppBarColorChange(checkBoxPreference);
@@ -73,11 +90,12 @@ public class PreferencesActivity extends AppCompatPreferenceActivity {
 //    }
 
 
+
     @Override
     protected void onDestroy() {
         Intent intent = new Intent();
         setResult(RESULT_OK, intent);
-        finish();
         super.onDestroy();
     }
+
 }
