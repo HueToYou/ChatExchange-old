0779ad6b34f384b1d0fcab3a41c7bf120fff44ac
michael <usernameinvalidexception@users.noreply.github.com>
2017-06-04 20:59:40 -0400

    Oops, forgot to rename the XML in last commit :D

--- app/src/main/java/com/huetoyou/chatexchange/ui/frags/HomeFragment.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/frags/HomeFragment.java
@@ -1,31 +1,14 @@
 package com.huetoyou.chatexchange.ui.frags;
 
-import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.AppCompatButton;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
 
 import com.huetoyou.chatexchange.R;
-import com.huetoyou.chatexchange.auth.AuthenticatorActivity;
-import com.huetoyou.chatexchange.ui.activity.MainActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeFragment extends Fragment {
 
@@ -40,7 +23,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_accounts, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
         getActivity().setTitle(getResources().getText(R.string.app_name));
 
