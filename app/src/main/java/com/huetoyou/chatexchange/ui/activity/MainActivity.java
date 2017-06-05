3cad7c5602e48121589fae8edb85beed9a935de4
Zachary Wander <zachary@techyteen.tk>
2017-06-05 15:03:11 -0400

    Fixed some coloring

--- app/src/main/java/com/huetoyou/chatexchange/ui/activity/MainActivity.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/activity/MainActivity.java
@@ -279,10 +279,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (chatroomsList != null) {
-            if (chatroomsList.getSelectedItemPosition() == 0) {
+            if (!mFragmentManager.findFragmentByTag("home").isDetached()) {
                 hueUtils.setActionBarColorDefault(this);
+                hueUtils.setAddChatFabColorDefault(this);
             } else if (!mSharedPrefs.getBoolean("dynamicallyColorBar", false)) {
                 hueUtils.setActionBarColorDefault(this);
+                hueUtils.setAddChatFabColorDefault(this);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
