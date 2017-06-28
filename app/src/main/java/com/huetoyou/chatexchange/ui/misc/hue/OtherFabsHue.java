eafc38f856c2805b4333e87e6941634e5e86168c
Zachary Wander <zachary@techyteen.tk>
2017-06-28 14:47:23 -0400

    Fix tinting

--- app/src/main/java/com/huetoyou/chatexchange/ui/misc/hue/OtherFabsHue.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/misc/hue/OtherFabsHue.java
@@ -1,5 +1,6 @@
 package com.huetoyou.chatexchange.ui.misc.hue;
 
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
@@ -29,10 +30,33 @@ public class OtherFabsHue
         }
 
         ColorStateList colorStateList = ColorStateList.valueOf(appBarColor);
+        setTints(colorStateList, activity);
+    }
 
+    public void setAddChatFabColorToSharedPrefsValue(AppCompatActivity activity)
+    {
+        //Grab an instance of SharedPrefs if we haven't already
+        if(mSharedPreferences == null)
+        {
+            mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
+        }
+
+        if (activity != null)
+        {
+            int hue = mSharedPreferences.getInt("fab_color", activity.getResources().getColor(R.color.colorAccent));
+
+            ColorStateList colorStateList = ColorStateList.valueOf(hue);
+            setTints(colorStateList, activity);
+
+            //showChatsTint(colorStateList, activity);
+        }
+    }
+
+    private void setTints(ColorStateList colorStateList, Activity activity) {
         FloatingActionMenu chatListMenu = activity.findViewById(R.id.chat_slide_menu);
         com.github.clans.fab.FloatingActionButton home = activity.findViewById(R.id.home_fab);
         com.github.clans.fab.FloatingActionButton addChat = activity.findViewById(R.id.add_chat_fab);
+        com.github.clans.fab.FloatingActionButton removeChats = activity.findViewById(R.id.remove_all_chats_fab);
 
         if (chatListMenu != null) {
             chatListMenu.setMenuButtonColorNormal(colorStateList.getDefaultColor());
@@ -56,60 +80,14 @@ public class OtherFabsHue
             addChat.setColorPressed(colorStateList.getDefaultColor());
             addChat.setImageDrawable(vectorDrawableCompat);
         }
-    }
 
-    public void setAddChatFabColorToSharedPrefsValue(AppCompatActivity activity)
-    {
-        //Grab an instance of SharedPrefs if we haven't already
-        if(mSharedPreferences == null)
-        {
-            mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
-        }
+        if (removeChats != null) {
+            VectorDrawableCompat vectorDrawableCompat = VectorDrawableCompat.create(activity.getResources(), R.drawable.ic_close_black_24dp, null);
+            vectorDrawableCompat.setTint(Color.rgb(255, 255, 255));
 
-        if (activity != null)
-        {
-            int hue = mSharedPreferences.getInt("fab_color", activity.getResources().getColor(R.color.colorAccent));
-
-            ColorStateList colorStateList = ColorStateList.valueOf(hue);
-
-            FloatingActionMenu chatListMenu = activity.findViewById(R.id.chat_slide_menu);
-            com.github.clans.fab.FloatingActionButton home = activity.findViewById(R.id.home_fab);
-            com.github.clans.fab.FloatingActionButton addChat = activity.findViewById(R.id.add_chat_fab);
-            com.github.clans.fab.FloatingActionButton removeChats = activity.findViewById(R.id.remove_all_chats_fab);
-
-            if (chatListMenu != null) {
-                chatListMenu.setMenuButtonColorNormal(colorStateList.getDefaultColor());
-                chatListMenu.setMenuButtonColorPressed(colorStateList.getDefaultColor());
-            }
-
-            if (home != null) {
-                VectorDrawableCompat vectorDrawableCompat = VectorDrawableCompat.create(activity.getResources(), R.drawable.ic_home_white_24dp, null);
-                vectorDrawableCompat.setTint(Color.rgb(255, 255, 255));
-
-                home.setColorNormal(colorStateList.getDefaultColor());
-                home.setColorPressed(colorStateList.getDefaultColor());
-                home.setImageDrawable(vectorDrawableCompat);
-            }
-
-            if (addChat != null) {
-                VectorDrawableCompat vectorDrawableCompat = VectorDrawableCompat.create(activity.getResources(), R.drawable.ic_add_black_24dp, null);
-                vectorDrawableCompat.setTint(Color.rgb(255, 255, 255));
-
-                addChat.setColorNormal(colorStateList.getDefaultColor());
-                addChat.setColorPressed(colorStateList.getDefaultColor());
-                addChat.setImageDrawable(vectorDrawableCompat);
-            }
-
-            if (removeChats != null) {
-                VectorDrawableCompat vectorDrawableCompat = VectorDrawableCompat.create(activity.getResources(), R.drawable.ic_close_black_24dp, null);
-                vectorDrawableCompat.setTint(Color.rgb(255, 255, 255));
-
-                removeChats.setColorNormal(colorStateList.getDefaultColor());
-                removeChats.setColorPressed(colorStateList.getDefaultColor());
-                removeChats.setImageDrawable(vectorDrawableCompat);
-            }
-
-            //showChatsTint(colorStateList, activity);
+            removeChats.setColorNormal(colorStateList.getDefaultColor());
+            removeChats.setColorPressed(colorStateList.getDefaultColor());
+            removeChats.setImageDrawable(vectorDrawableCompat);
         }
     }
 }
