9d6edfd8a1d25426fc8695fd5d656f6f2f0b7e1f
michael <usernameinvalidexception@users.noreply.github.com>
2017-06-27 21:14:04 -0400

    Comments in ThemeHue

--- app/src/main/java/com/huetoyou/chatexchange/ui/misc/hue/ThemeHue.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/misc/hue/ThemeHue.java
@@ -6,36 +6,49 @@ import android.preference.PreferenceManager;
 
 import com.huetoyou.chatexchange.R;
 
+/*
+ * This class is for setting the theme of MainActivity
+ */
+
 public class ThemeHue
 {
-    /*
-     * This class is for setting the theme of MainActivity
-     */
-
     private SharedPreferences mSharedPreferences = null;
 
+    /*
+     * Only call this method from onCreate(), calling it afterwards will not have any effect
+     */
     public void setTheme(Activity activity)
     {
+        //Grab an instance of SharedPrefs if we haven't already
         if(mSharedPreferences == null)
         {
             mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
         }
 
+        //Grab the value of the darkTheme preference
         boolean desiredThemeIsDark = mSharedPreferences.getBoolean("darkTheme", false);
 
+        //We only need to explicitly set the theme if the user wants the dark theme, as light is the default
         if(desiredThemeIsDark)
         {
+            //NB: this *must* be called before super.onCreate(), else weird things happen
             activity.setTheme(R.style.DarkTheme);
         }
     }
 
+    /*
+     * Do *not* call this method from onCreate(); used for changing the theme on-the-fly; you should call this in onResume()
+     */
     public void setThemeOnResume(Activity activity, boolean oncreatejustcalled)
     {
+        //Grab an instance of SharedPrefs if we haven't already
         if(mSharedPreferences == null)
         {
             mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
         }
 
+        //Check to see whether or not the activity must be restarted due to a requested theme change.
+        //Requires the oncreatejustcalled parameter to prevent setting the theme twice, as onResume() is called after onCreate() finishes
         if(mSharedPreferences.getBoolean("FLAG_restartMain", false) && !oncreatejustcalled)
         {
             mSharedPreferences.edit().putBoolean("FLAG_restartMain", false).apply();
