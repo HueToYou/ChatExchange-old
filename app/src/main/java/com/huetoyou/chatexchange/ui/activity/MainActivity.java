3c6f93ba92b8eb80116269c4fc3c26601e18bd46
Zachary Wander <zachary@techyteen.tk>
2017-06-29 08:26:38 -0400

    Slow down animation speed on FAMs

--- app/src/main/java/com/huetoyou/chatexchange/ui/activity/MainActivity.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/activity/MainActivity.java
@@ -23,6 +23,7 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.graphics.drawable.VectorDrawableCompat;
