f999624853227966d8d09dded89b3948aa25fcbb
UsernameInvalidException <usernameinvalidexception@users.noreply.github.com>
2017-07-10 21:08:23 -0400

    ARR --> source

---
+++ Spotlight-library/src/main/java/com/wooplr/spotlight/utils/SpotlightListener.java
@@ -0,0 +1,12 @@
+package com.wooplr.spotlight.utils;
+
+
+/**
+ * Created by jitender on 10/06/16.
+ */
+
+public interface SpotlightListener {
+
+    void onUserClicked(String spotlightViewId);
+    void onFinishedDrawingSpotlight();
+}
