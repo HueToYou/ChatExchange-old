b54e5c378e29cdb9a8fedbf00ea69a719f968c54
UsernameInvalidException <usernameinvalidexception@users.noreply.github.com>
2017-07-15 17:42:18 -0400

    Implement auto refresh :D

--- app/src/main/java/com/huetoyou/chatexchange/ui/activity/OfflineActivity.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/activity/OfflineActivity.java
@@ -20,20 +20,72 @@ public class OfflineActivity extends Activity
         ThemeHue.setTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_offline);
+        startAsyncPingThread();
     }
 
     public void checkOnlineStatus(View v)
     {
         if(Utils.areWeOnANetwork(this) && Utils.areWeOnline())
         {
-            Toast.makeText(this, "Connection established, reloading app :)", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, SplashActivity.class);
-            startActivity(intent);
-            finish();
+            reloadApp();
         }
         else
         {
             Toast.makeText(this, "You're still offline :\\", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void reloadApp()
+    {
+        Toast.makeText(OfflineActivity.this, "Connection established, reloading app :)", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, SplashActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void startAsyncPingThread()
+    {
+        try
+        {
+            Thread thread = new Thread(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    while (true)
+                    {
+                        if(Utils.areWeOnline())
+                        {
+                            OfflineActivity.this.runOnUiThread(new Runnable()
+                            {
+                                @Override
+                                public void run()
+                                {
+                                    reloadApp();
+                                }
+                            });
+                            break;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                Thread.sleep(250);
+                            }
+                            catch (InterruptedException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            });
+
+            thread.start();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
         }
     }
 }
