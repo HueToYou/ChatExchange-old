3cffaf13d07d4f355598b00bd75f4b06620a98e5
UsernameInvalidException <usernameinvalidexception@users.noreply.github.com>
2017-08-05 14:56:47 -0400

    Ooops, forgot to change the commit message :\
    *sigh* well that last commit was the start of implementing an sqlite database

--- app/src/main/java/com/huetoyou/chatexchange/ui/misc/HueDatabase.java
+++ app/src/main/java/com/huetoyou/chatexchange/ui/misc/HueDatabase.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import java.util.ArrayList;
 
 public class HueDatabase extends SQLiteOpenHelper
